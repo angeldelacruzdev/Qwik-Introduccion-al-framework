c0aefb75432f0623144f955f3fe632c3e24b50e7
Ángel De La Cruz <angeldelacruzdev@gmail.com>
2023-05-27 12:02:09 -0400

    Funciones en un component

--- seccion-2/src/routes/index.tsx
+++ seccion-2/src/routes/index.tsx
@@ -1,8 +1,14 @@
-import { component$, useSignal, useStore } from "@builder.io/qwik";
+import { component$, useSignal, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   const pokemondId = useSignal(1);
+
+  const changePokeminId = $((value: number) => {
+    if (pokemondId.value + value <= 0) return;
+
+    pokemondId.value += value;
+  });
 
   return (
     <>
@@ -10,10 +16,18 @@ export default component$(() => {
       <span class="text-9xl">{pokemondId}</span>
 
       {/* TODO: crear imagen */}
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemondId.value}.png`}
+        alt=""
+      />
 
       <div class="flex gap-3 mt-2">
-        <button  class="btn btn-primary">Anterior</button>
-        <button class="btn btn-primary">Siguientes</button>
+        <button onClick$={() => changePokeminId(-1)} class="btn btn-primary">
+          Anterior
+        </button>
+        <button onClick$={() => changePokeminId(+1)} class="btn btn-primary">
+          Siguientes
+        </button>
       </div>
     </>
   );
