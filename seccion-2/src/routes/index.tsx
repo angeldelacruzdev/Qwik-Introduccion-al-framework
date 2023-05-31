0cc6c00269316494894179c497ec55c8c3c75510
Ángel De La Cruz <angeldelacruzdev@gmail.com>
2023-05-31 15:09:46 -0400

    Fin seccion 2

--- seccion-2/src/routes/index.tsx
+++ seccion-2/src/routes/index.tsx
@@ -1,8 +1,11 @@
 import { component$, useSignal, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { PokemonImage } from "~/components/pokemons/pokemon-image";
 
 export default component$(() => {
   const pokemondId = useSignal(1);
+  const showBackImage = useSignal(false);
+  const isVisibleImage = useSignal(true);
 
   const changePokeminId = $((value: number) => {
     if (pokemondId.value + value <= 0) return;
@@ -15,10 +18,10 @@ export default component$(() => {
       <span class="text-2xl">Buscador simple</span>
       <span class="text-9xl">{pokemondId}</span>
 
-      {/* TODO: crear imagen */}
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemondId.value}.png`}
-        alt=""
+      <PokemonImage
+        id={pokemondId.value}
+        backIimage={showBackImage.value}
+        isVisible={isVisibleImage.value}
       />
 
       <div class="flex gap-3 mt-2">
@@ -26,7 +29,19 @@ export default component$(() => {
           Anterior
         </button>
         <button onClick$={() => changePokeminId(+1)} class="btn btn-primary">
-          Siguientes
+          Siguiente
+        </button>
+        <button
+          onClick$={() => (showBackImage.value = !showBackImage.value)}
+          class="btn btn-primary"
+        >
+          Voltear
+        </button>
+        <button
+          onClick$={() => (isVisibleImage.value = !isVisibleImage.value)}
+          class="btn btn-primary"
+        >
+          Revelar
         </button>
       </div>
     </>
