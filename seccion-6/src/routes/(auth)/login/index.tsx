b146d21a267a306241ee3961c8b6fda1012c471b
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2023-08-05 10:51:38 -0400

    Fin de la sección 7 en el directorio de la sección 6

--- seccion-6/src/routes/(auth)/login/index.tsx
+++ seccion-6/src/routes/(auth)/login/index.tsx
@@ -1,80 +1,58 @@
-import {
-  $,
-  component$,
-  useComputed$,
-  useStore,
-  useStylesScoped$,
-} from "@builder.io/qwik";
+import { $, component$, useStylesScoped$ } from "@builder.io/qwik";
 
 import styles from "./login.css?inline";
+import { Form, routeAction$, z, zod$ } from "@builder.io/qwik-city";
+
+export const useLoginUserAction = routeAction$(
+  (data, { cookie, redirect }) => {
+    const { email, password } = data;
+    console.log({ email, password });
+
+    if (email === "angeldelacruzdev@gmail.com" && password === "admin123456") {
+      cookie.set("jwt", "jwt_000012121a2s1d2a1d2a1d2a1d2a1da21da2s1d", {
+        secure: true,
+        path: "/",
+      });
+      redirect(302, "/");
+    }
+
+    return {
+      success: false,
+      jwt: null,
+    };
+  },
+  zod$({
+    email: z.string().email("El email no es valido."),
+    password: z.string().min(6, "Minimo 6 caracteres."),
+  })
+);
 
 export default component$(() => {
   useStylesScoped$(styles);
 
-  const formState = useStore({
-    email: "",
-    password: "",
-    formPosted: false,
-  });
-
-  const emailError = useComputed$(() => {
-    if (formState.email.includes("@")) return "";
-
-    return "not-valid";
-  });
-
-  const passwordError = useComputed$(() => {
-    if (formState.password.length >= 6) return "";
-
-    return "not-valid";
-  });
-
-  const isFormValid = useComputed$(() => {
-    if (emailError.value === "not-valid" || passwordError.value === "not-valid")
-      return false;
-
-    return true;
-  });
-
-  const onSubmit = $(() => {
-    formState.formPosted = true;
-    const { email, password } = formState;
-    console.log({ email, password });
-  });
+  const action = useLoginUserAction();
 
   return (
-    <form onSubmit$={onSubmit} class="login-form" preventdefault:submit>
+    <Form action={action} class="login-form mt-5">
       <div class="relative">
-        <input
-          onInput$={(ev) =>
-            (formState.email = (ev.target as HTMLInputElement).value)
-          }
-          name="email"
-          type="text"
-          class={formState.formPosted ? emailError.value : ""}
-          placeholder="Email address"
-        />
+        <input name="email" type="text" placeholder="Email address" />
         <label for="email">Email Address</label>
       </div>
       <div class="relative">
-        <input
-          onInput$={(ev) =>
-            (formState.password = (ev.target as HTMLInputElement).value)
-          }
-          name="password"
-          type="password"
-          placeholder="Password"
-          class={formState.formPosted ? passwordError.value : ""}
-        />
+        <input name="password" type="password" placeholder="Password" />
         <label for="password">Password</label>
       </div>
       <div class="relative">
-        <button disabled={!isFormValid.value} type="submit">
-          Ingresar
-        </button>
+        <button type="submit">Ingresar</button>
       </div>
 
-      <code>{JSON.stringify(formState, undefined, 2)}</code>
-    </form>
+      <p>
+        {action.value?.success && (
+          <code>Atenticado: Token = {action.value.jwt} </code>
+        )}
+      </p>
+
+      <code>{JSON.stringify(action.value, undefined, 2)}</code>
+    </Form>
   );
 });
--- seccion-6/src/routes/dashboard/index.tsx
+++ seccion-6/src/routes/dashboard/index.tsx
@@ -1,4 +1,19 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+("");
+
+export const useChackAuthCookie = routeLoader$(({ cookie, redirect }) => {
+  const jwtCookie = cookie.get("jwt");
+
+  if (jwtCookie) {
+    console.log("Cookie valido", jwtCookie);
+
+    return;
+  }
+
+
+  redirect(302, "/")
+});
 
 export default component$(() => {
   return (
